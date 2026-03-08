3adb589591a0a65fd1e375fadc52cefbb910a573
AndreaDevelope <andrea_develope@outlook.it>
2026-03-08 01:37:22 +0100

    feat(stories): complete mobile stories page UI and basic interactions

--- src/app/features/stories/pages/stories-list-page/stories-list.ts
+++ src/app/features/stories/pages/stories-list-page/stories-list.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { Story } from '../../models/story';
 import { StoryCard } from '../../components/story-card-components/story-card';
 import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [StoryCard, RouterLink],
+  imports: [StoryCard, RouterLink, NgClass],
   templateUrl: './stories-list.html',
   styleUrl: './stories-list.scss',
 })
 export class StoriesList {
+  manifestoIsShowMore = false;
+  selectedCategory = 'tutte';
   stories: Story[] = [
     {
-      category: 'Porcodio',
+      category: 'porcodio',
       district: 'Sottocasa',
       date: '25-02-2026',
       title: 'PORCODIO',
@@ -21,7 +24,7 @@ export class StoriesList {
         'Porcodio e la madonna e tutti gli angeli in colonna li ho contati son 31 porco dio ne manca 1',
     },
     {
-      category: 'Porcodio2',
+      category: 'porcodio2',
       district: 'Sottocasa',
       date: '25-02-2026',
       title: 'PORCODIO2',
@@ -29,7 +32,7 @@ export class StoriesList {
         'Porcodio e la madonna e tutti gli angeli in colonna li ho contati son 31 porco dio ne manca 1',
     },
     {
-      category: 'Porcodio3',
+      category: 'porcodio3',
       district: 'Sottocasa',
       date: '25-02-2026',
       title: 'PORCODIO3',
@@ -37,7 +40,7 @@ export class StoriesList {
         'Porcodio e la madonna e tutti gli angeli in colonna li ho contati son 31 porco dio ne manca 1',
     },
     {
-      category: 'Porcodio4',
+      category: 'porcodio4',
       district: 'Sottocasa',
       date: '25-02-2026',
       title: 'PORCODIO4',
@@ -45,4 +48,33 @@ export class StoriesList {
         'Porcodio e la madonna e tutti gli angeli in colonna li ho contati son 31 porco dio ne manca 1',
     },
   ];
+
+  filtredStories: Story[] = this.stories;
+
+  onSearch(event: Event) {
+    const value = (event.target as HTMLInputElement).value.toLocaleLowerCase();
+
+    if (value === 'tutte' || value === '') {
+      this.filtredStories = this.stories;
+      return;
+    }
+
+    this.filtredStories = this.stories.filter((s) => s.category.includes(value));
+  }
+
+  filterCategory(category: string) {
+    if (category === 'tutte' || category === '') {
+      this.filtredStories = this.stories;
+      this.selectedCategory = category;
+      return;
+    }
+    this.filtredStories = this.stories.filter((s) =>
+      s.category.includes(category.toLocaleLowerCase()),
+    );
+    this.selectedCategory = category;
+  }
+
+  manifestoShowMoreToggle() {
+    this.manifestoIsShowMore = !this.manifestoIsShowMore;
+  }
 }
