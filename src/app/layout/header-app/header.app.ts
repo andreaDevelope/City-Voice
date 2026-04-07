3983095838e1f8e6002205d5766caaee4c169561
Andrea Ceccarelli <cons.andrea.ceccarelli@cy4gate.com>
2026-04-07 10:46:44 +0200

    wip: refactor responsive editorial settings page

--- src/app/layout/header-app/header.app.ts
+++ src/app/layout/header-app/header.app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   standalone: true,
   selector: 'app-header-app',
   templateUrl: './header.app.html',
   styleUrls: ['./header.app.scss'],
+  imports: [RouterLink],
 })
 export class HeaderApp {}
